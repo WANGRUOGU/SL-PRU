6d75d5bbeb3962868e53d6ceb0acee6304e2d888
WANGRUOGU <49007977+WANGRUOGU@users.noreply.github.com>
2023-08-08 19:09:37 -0400

    Add files via upload

--- demo.m
+++ demo.m
@@ -1,20 +1,30 @@
-%% Loading E.coli reference images and an unknown mixture image
-clear variables
-load EcoliEndMat.mat
-%% Parameters
+% setup
+close all
+addpath(genpath(pwd))
+warning('off')
+filename = 'mixture.tif'; % image file name
+image = readMultipageTiff(filename);
+load endmembers.mat % import fluorophore spectral signatures
+
+% tuning parameters
+% this step can be ignored and directly set the values for lambda1 and
+% lambda2
+R = size(E, 2); % number of endmembers
+ref = cell(1, R);
+for r = 1:R % load reference images for tuning
+    ref{r} = readMultipageTiff([Fluorophores{r}, '.tif']);
+end
 lam_seq = @(lam_ub, lam_lb, lam_range) ...
     [0,exp(log(lam_lb):((log(lam_ub)-log(lam_lb))/lam_range):log(lam_ub))];
-lam = lam_seq(10, .1, 8);
-sz_sam = 1e3; % sample size (large for accuracy/small for time conssumming)
-%% Endmembers extraction
-E_Ecoli = PNMF(ref_Ecoli);
-%% Tuning parameters
-[l1_PR, l2_PR, l1_NLS, l2_NLS, l_SNLS, prop]...
-    = Tuning(ref_Ecoli, E_Ecoli, Fluorophores, lam, lam, sz_sam);
-%% Unmixing a real mixed biological image
-A_NLS = UnmixSpIm(mixture, E_Ecoli, 'NLS', 0, 0);
-A_SNLS = UnmixSpIm(mixture, E_Ecoli, 'SNLS', 0, l_SNLS);
-A_SLNLS = UnmixSpIm(mixture, E_Ecoli, 'SLNLS', l1_NLS, l2_NLS);
-A_SLPRU = UnmixSpIm(mixture, E_Ecoli, 'SLPRU', l1_PR, l2_PR);
-%% Estimated abundance matrices
-FigSpIm(mixture, A_NLS, A_SNLS, A_SLNLS, A_SLPRU, Fluorophores)
+lam = lam_seq(5, .1, 3);
+sz_sam = 1e0;
+[l1_PR, l2_PR, prop] = Tuning(ref, E, Fluorophores, lam, lam, sz_sam);
+
+% SL-PRU unmixing
+A = UnmixSpIm(image, E, 'SLPRU', l1_PR, l2_PR);
+
+% save the abundances
+imwrite(A(:, :, 1), ['./result/Unmixed_', filename]);
+for r = 2:R
+    imwrite(A(:, :, r), ['./result/Unmixed_', filename], 'WriteMode' , 'append');
+end
